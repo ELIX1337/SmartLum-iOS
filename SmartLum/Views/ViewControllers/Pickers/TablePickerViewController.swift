09bd331c018be280ce8e2ddf9127c84ecd2e857d
Tim <timur-sibay@mail.ru>
2021-07-07 14:32:49 +0500

    Updated TablePickerViewController

--- SmartLum/Views/ViewControllers/Pickers/TablePickerViewController.swift
+++ SmartLum/Views/ViewControllers/Pickers/TablePickerViewController.swift
@@ -8,77 +8,56 @@
 
 import UIKit
 
-protocol TablePickerDelegate {
-    func getSelection(at index: Int, in array: [Int:String])
-}
-
-class TablePickerViewController: UIViewController,
-                                     UITableViewDelegate,
-                                     UITableViewDataSource {
+class TablePickerViewController: PopupPickerViewController {
     
-    var tableView:UITableView = UITableView()
-    var delegate:TablePickerDelegate?
-    var dataArray:(array: [Int:String], index: Int?)?
-    private var lastSelection:IndexPath?
-
+    public var delegate: UITableViewDelegate?
+    public var dataSource: UITableViewDataSource?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.largeTitleDisplayMode = .never
-        tableView = UITableView(frame: UIScreen.main.bounds, style: UITableView.Style.plain)
-        self.tableView.tableFooterView = UIView()
-        self.tableView.dataSource = self
-        self.tableView.delegate = self
-        self.view.addSubview(tableView)
-        tableView.reloadData()
-    }
-    
-    // MARK: - TableView protocol
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let arraySize = dataArray?.array.count {
-            return arraySize
-        } else {
-            return 0
-        }
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = {
-         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
-             return UITableViewCell(style: .default, reuseIdentifier: "cell")
-             }
-             return cell
-         }()
-        cell.textLabel?.text = dataArray?.array[indexPath.row + 1]?.localized
-        
-        if let a = dataArray?.index {
-            if indexPath.row == a - 1 {
-                cell.accessoryType = .checkmark
-                lastSelection = indexPath
-            }
-        }
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        delegate?.getSelection(at: indexPath.row + 1, in: dataArray!.array)
-        if let index = lastSelection {
-            if index != indexPath {
-                tableView.cellForRow(at: index)?.accessoryType = .none
-            }
-        }
-        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        lastSelection = indexPath
-    }
-    
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
-    }
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.cellLayoutMarginsFollowReadableWidth = false
+        tableView.backgroundColor = UIColor.clear
+        tableView.tableFooterView = UIView()
+        tableView.allowsMultipleSelection = false
 
+        self.containerView.addSubview(tableView)
+        NSLayoutConstraint(item: tableView,
+                           attribute: NSLayoutConstraint.Attribute.bottom,
+                           relatedBy: NSLayoutConstraint.Relation.equal,
+                           toItem: self.containerView,
+                           attribute: NSLayoutConstraint.Attribute.bottomMargin,
+                           multiplier: 1.0,
+                           constant: 0.0).isActive = true
+        NSLayoutConstraint(item: tableView,
+                           attribute: NSLayoutConstraint.Attribute.top,
+                           relatedBy: NSLayoutConstraint.Relation.equal,
+                           toItem: self.containerView,
+                           attribute: NSLayoutConstraint.Attribute.top,
+                           multiplier: 1.0,
+                           constant: 0.0).isActive = true
+        NSLayoutConstraint(item: tableView,
+                           attribute: NSLayoutConstraint.Attribute.leading,
+                           relatedBy: NSLayoutConstraint.Relation.equal,
+                           toItem: self.containerView,
+                           attribute: NSLayoutConstraint.Attribute.leading,
+                           multiplier: 1.0,
+                           constant: 0.0).isActive = true
+        NSLayoutConstraint(item: tableView,
+                           attribute: NSLayoutConstraint.Attribute.trailing,
+                           relatedBy: NSLayoutConstraint.Relation.equal,
+                           toItem: self.containerView,
+                           attribute: NSLayoutConstraint.Attribute.trailing,
+                           multiplier: 1.0,
+                           constant: 0.0).isActive = true
+        tableView.dataSource = dataSource
+        tableView.delegate = delegate
+        tableView.reloadData()
+        print("sdf \(String(describing: tableView.dataSource))")
+    }
+    
 }
 
 extension Array where Element: Equatable {
