f7a5a4bb483f7ccb88f93b91a890edb8a4d43c6e
Tim <timur-sibay@mail.ru>
2021-10-22 10:49:58 +0500

    - Fixed multiple peripheral instances - Now viewModel is BasePeripheralDelegate - Added observable init fields - TableViewModel now separated to 3 different models (ready, setup, settings) - Added tableView types - Refactored row/section hiding and showing

--- SmartLum/Models/PeripheralViewModel.swift
+++ SmartLum/Models/PeripheralViewModel.swift
@@ -8,120 +8,156 @@
 
 import UIKit
 
-class PeripheralViewModel: NSObject {
+protocol PeripheralViewModelDelegate {
+    func peripheralInitState(isInitialized: Bool)
+    func peripheralDidConnect()
+    func peripheralDidDisconnect(reason: Error?)
+    func peripheralIsReady()
+    func peripheralOnDFUMode()
+}
+
+@objc class PeripheralViewModel: NSObject {
     
-    public var basePeripheral: BasePeripheral
+    public var basePeripheral: BasePeripheral!
     public var tableView: UITableView
+    public var delegate: PeripheralViewModelDelegate
     public var selection: (PeripheralRow) -> Void
     public var dataModel = PeripheralDataModel()
-    public var tableViewModel: PeripheralTableViewModel!
     public var hiddenIndexPath = HiddenIndexPath()
-
-    init(_ withTableView:   UITableView,
+    
+    public var peripheralReadyTableViewModel: PeripheralTableViewModel?
+    public var peripheralSetupTableViewModel: PeripheralTableViewModel?
+    public var peripheralSettingsTableViewModel: PeripheralTableViewModel?
+    
+    public var tableViewModel: PeripheralTableViewModel! {
+        isInitialized ? peripheralReadyTableViewModel : peripheralSetupTableViewModel
+    }
+    public var isConnected:    Bool { basePeripheral.isConnected }
+    public var peripheralName: String { basePeripheral.name }
+    
+    @objc dynamic public var readyToWriteInitData: Bool = false
+    @objc dynamic public var isInitialized: Bool = true
+    
+    init(_ withTableView:  UITableView,
          _ withPeripheral: BasePeripheral,
-         _ baseDelegate:   BasePeripheralDelegate,
+         _ delegate:       PeripheralViewModelDelegate,
          _ selected:       @escaping (PeripheralRow) -> Void) {
         self.tableView = withTableView
         self.selection = selected
-        self.basePeripheral = TorcherePeripheral.init(withPeripheral.peripheral, withPeripheral.centralManager)
-        super.init()
-        self.basePeripheral.baseDelegate = baseDelegate
+        self.delegate  = delegate
+        self.basePeripheral = withPeripheral
         self.basePeripheral.connect()
+        super.init()
+        self.basePeripheral.baseDelegate = self
+    }
+    
+    internal func getTableViewModel(type: PeripheralTableViewModel.TableViewType) -> PeripheralTableViewModel {
+        switch type {
+        case .ready:    return peripheralReadyTableViewModel!
+        case .settings: return peripheralSettingsTableViewModel!
+        case .setup:    return peripheralSetupTableViewModel!
+        }
     }
     
     public func reloadCell(for row: PeripheralRow, with animation: UITableView.RowAnimation) {
-        if let indexPath = tableViewModel.getIndexPath(forRow: row) {
+        if let indexPath = getTableViewModel(type: tableView.tableViewType).getIndexPath(forRow: row) {
             tableView.reloadRows(at: [indexPath], with: animation)
         } else {
             tableView.reloadData()
         }
     }
     
-    public func hideRows(rows: [PeripheralRow]) {
-        //tableView.beginUpdates()
-            for row in rows {
-                if let rowIndex = tableViewModel.getIndexPath(forRow: row) {
-                    hiddenIndexPath.row.append(rowIndex)
-                }
-            }
-        //tableView.endUpdates()
+    
+    public func hideCell(rows: [PeripheralRow], rowsSection: [PeripheralRow]?) {
+        hideRows(rows: rows)
+        if let section = rowsSection {
+            hideSections(of: section)
+        }
     }
     
-    public func showRows(rows: [PeripheralRow]?) {
-        //tableView.beginUpdates()
-            guard let array = rows else {
-                hiddenIndexPath.row.removeAll()
-                tableView.endUpdates()
-                return
-            }
-            for row in array {
-                if let rowIndex = tableViewModel.getIndexPath(forRow: row) {
-                    hiddenIndexPath.row = hiddenIndexPath.row.filter { $0 != rowIndex }
-                }
-            }
-        //tableView.endUpdates()
+    private func hideRows(rows: [PeripheralRow]) {
+        let indexPaths = rows.map { tableViewModel.getIndexPath(forRow: $0) }.compactMap { $0 }
+        showRows(rows: nil)
+        showSections(of: nil)
+        hiddenIndexPath.row = indexPaths
+        tableView.reloadRows(at: indexPaths, with: .top)
     }
     
-    public func hideSections(of: [PeripheralRow]) {
-        //tableView.beginUpdates()
-            for section in of {
-                if let sectionIndex = tableViewModel.getIndexPath(forRow: section)?.section {
-                    hiddenIndexPath.section.append(sectionIndex)
-                }
+    private func showRows(rows: [PeripheralRow]?) {
+        guard let array = rows else {
+            hiddenIndexPath.row.removeAll()
+            tableView.reloadData()
+            return
+        }
+        for row in array {
+            if let rowIndex = tableViewModel.getIndexPath(forRow: row) {
+                hiddenIndexPath.row = hiddenIndexPath.row.filter { $0 != rowIndex }
+                tableView.reloadRows(at: [rowIndex], with: .top)
             }
-        //tableView.endUpdates()
+        }
     }
     
-    public func showSections(of: [PeripheralRow]?) {
-        //tableView.beginUpdates()
-            guard let array = of else {
-                hiddenIndexPath.section.removeAll()
-                tableView.endUpdates()
-                return
-            }
-            for section in array {
-                if let sectionIndex = tableViewModel.getIndexPath(forRow: section)?.section {
-                    hiddenIndexPath.section = hiddenIndexPath.section.filter { $0 != sectionIndex }
-                }
-            }
-        //tableView.endUpdates()
+    private func hideSections(of: [PeripheralRow]) {
+        let indexPaths = of.map { tableViewModel.getIndexPath(forRow: $0) }.compactMap { $0?.section }
+        hiddenIndexPath.section = indexPaths
+        hiddenIndexPath.section.forEach { tableView.reloadSections(IndexSet(integer: $0), with: .top) }
     }
     
-    public func cellCallback(fromRow: PeripheralRow, withValue: Any?) { }
+    private func showSections(of: [PeripheralRow]?) {
+        guard let array = of else {
+            hiddenIndexPath.section.removeAll()
+            tableView.reloadData()
+            return
+        }
+        for section in array {
+            if let sectionIndex = tableViewModel.getIndexPath(forRow: section)?.section {
+                hiddenIndexPath.section = hiddenIndexPath.section.filter { $0 != sectionIndex }
+                tableView.reloadSections(IndexSet(integer: sectionIndex), with: .none)
+            }
+        }
+    }
     
-    public func peripheralIsConnected() -> Bool { basePeripheral.isConnected }
-    
-    public func getPeripheralName() -> String { basePeripheral.name }
+    public func cellDataCallback(fromRow: PeripheralRow, withValue: Any?) { }
     
     public func disconnect() { basePeripheral.disconnect() }
     
 }
 
+extension UITableView {
+    var tableViewType: PeripheralTableViewModel.TableViewType {
+        get {
+            return PeripheralTableViewModel.TableViewType.init(rawValue: self.tag) ?? .ready
+        }
+        set {
+            self.tag = newValue.rawValue
+        }
+    }
+}
+
 extension PeripheralViewModel: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        self.tableViewModel.sections.count
+        self.getTableViewModel(type: tableView.tableViewType).sections.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        self.tableViewModel.sections[section].rows.count
+        self.getTableViewModel(type: tableView.tableViewType).sections[section].rows.count
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        self.tableViewModel.sections[section].headerText.localized
+        self.getTableViewModel(type: tableView.tableViewType).sections[section].headerText.localized
     }
     
     func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
-        self.tableViewModel.sections[section].footerText.localized
+        self.getTableViewModel(type: tableView.tableViewType).sections[section].footerText.localized
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = tableViewModel.sections[indexPath.section].rows[indexPath.row]
+        let row = getTableViewModel(type: tableView.tableViewType).sections[indexPath.section].rows[indexPath.row]
         tableView.register(UINib.init(nibName: row.nibName, bundle: nil), forCellReuseIdentifier: row.cellReuseID)
         let cell = tableView.dequeueReusableCell(withIdentifier: row.cellReuseID, for: indexPath) as! BaseTableViewCell
-        //cell.configure(title: row.name, value: row.cellValue(from: dataModel))
         row.setupCell(cell: cell, with: dataModel)
-        cell.returnValue = { value in self.cellCallback(fromRow: row, withValue: value) }
+        cell.returnValue = { value in self.cellDataCallback(fromRow: row, withValue: value) }
         return cell
     }
     
@@ -155,9 +191,46 @@ extension PeripheralViewModel: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        self.selection(tableViewModel.sections[indexPath.section].rows[indexPath.row])
+        self.selection(getTableViewModel(type: tableView.tableViewType).sections[indexPath.section].rows[indexPath.row])
     }
-
+    
 }
 
-
+extension PeripheralViewModel: BasePeripheralDelegate {
+    
+    func peripheralError(code: Int) {
+        dataModel.errorCode = code
+        if (peripheralReadyTableViewModel != nil) {
+            if (!peripheralReadyTableViewModel!.sections.contains(PeripheralTableViewModel.errorSection)) {
+                tableView.beginUpdates()
+                peripheralReadyTableViewModel!.sections.insert(PeripheralTableViewModel.errorSection, at: 0)
+                tableView.insertSections(IndexSet(integer: 0), with: .top)
+                tableView.endUpdates()
+            }
+        }
+    }
+    
+    func peripheralInitState(isInitialized: Bool) {
+        delegate.peripheralInitState(isInitialized: isInitialized)
+    }
+    
+    func peripheralDidConnect() {
+        delegate.peripheralDidConnect()
+    }
+    
+    func peripheralDidDisconnect(reason: Error?) {
+        delegate.peripheralDidDisconnect(reason: reason)
+    }
+    
+    func peripheralIsReady() {
+        delegate.peripheralIsReady()
+    }
+    
+    func peripheralFirmwareVersion(_ version: Int) {
+        dataModel.firmwareVersion = version
+    }
+    
+    func peripheralOnDFUMode() {
+        delegate.peripheralOnDFUMode()
+    }
+}
